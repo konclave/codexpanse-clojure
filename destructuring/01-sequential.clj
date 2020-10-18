2fb5449c70f07f4c4b8181cd7af5c92d6d1c3621
Ivan Vasilyev <vianomore@gmail.com>
2020-10-18 23:25:39 +0300

    finish sequential destructuring

--- destructuring/01-sequential.clj
+++ destructuring/01-sequential.clj
@@ -1,3 +1,5 @@
 (let [[p e t] "pet"]
   (map clojure.string/upper-case [p e t]))
 
+(let [[v2 [v2url v2text] v3 [v3url v3text]] ["v2" ["https://go.io" "rf23fgg"] "v3" ["https://beta.go.io" "2k32k23k"]]]
+  (println [[v2 v2url v2text] [v3 v3url v3text]]))
