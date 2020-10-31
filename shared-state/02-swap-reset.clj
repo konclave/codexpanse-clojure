46f4bc8eac4562d2b70f5466c86f152adad1fc19
Ivan Vasilyev <vianomore@gmail.com>
2020-10-31 23:56:48 +0300

    problems 1,2 of swap reset of shared state

---
+++ shared-state/02-swap-reset.clj
@@ -0,0 +1,12 @@
+(require '[clojure.string :as str])
+
+(defn rev-addr [email]
+  (let [[uname domain] (str/split email #"@")]
+    (let [[flevel slevel] (str/split domain #"\.")]
+      (apply str [flevel "@" uname "." slevel]))))
+
+(def users (atom {"Kane" "kane@ucl.ca"
+            "Jerlamarel" "isee@you.com"
+                  "Jun" nil}))
+
+(swap! users assoc "Jerlamarel" (rev-addr (get (deref users) "Jerlamarel")))
